3f32521eaa3611f30d17b99c061bff44690abfff
aajetechnology <aajetechnology@gmail.com>
2026-02-03 10:57:48 +0100

    config.py

--- config.py
+++ config.py
@@ -7,5 +7,13 @@ class Config:
     SECRET_KEY = os.getenv('SECRET_KEY') or 'dev-key-change-this-in-prod'
     SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL') or 'sqlite:///studbot.db'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
+
+    SQLALCHEMY_ENGINE_OPTIONS = {
+        "pool_pre_ping": True,
+        "pool_recycle": 300,
+    }
+
+
+
     UPLOAD_FOLDER = os.path.join(os.path.abspath(os.path.dirname(__file__)), 'uploads')
     MAX_CONTENT_LENGTH = 200 * 1024 * 1024
